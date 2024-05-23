48b5f5c42506d6e17639f5ea79092fe2e21f3a32
Vladyslav Yamko <vebleb01@gmail.com>
2024-05-23 10:26:52 +0300

    change navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,18 @@
+import { Button, ButtonGroup } from "@mui/material";
 import Link from "next/link";
 
 export default function NavBar() {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link href="/search-movement">Search Movement</Link>
-        </li>
-        <li>
-          <Link href="/bank-account">Bank Account</Link>
-        </li>
-      </ul>
+      <ButtonGroup sx={{ margin: "20px" }}>
+        <Link href="/search-movement">
+          <Button>Search Movement</Button>
+        </Link>
+
+        <Link href="/bank-account">
+          <Button>Bank Account</Button>
+        </Link>
+      </ButtonGroup>
     </nav>
   );
 }
